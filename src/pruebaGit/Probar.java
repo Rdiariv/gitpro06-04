2e61f1d0c68afb2c87a1df6246d75af0ab8e03c2
Ruben <rdr1982@gmail.com>
2022-04-06 17:46:27 +0100

    primer commit

---
+++ src/pruebaGit/Probar.java
@@ -0,0 +1,16 @@
+package pruebaGit;
+
+public class Probar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String nombre="Ruben";
+		System.out.println(nombre);
+		System.out.println("otro commit");
+		System.out.println("más commits");
+		
+
+	}
+
+}
